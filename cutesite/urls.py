08dbc5bd2ebc57a79d7f690cd0a45ea7415ae98f
Jeff Dlouhy <jeff.dlouhy@gmail.com>
2009-06-14 17:53:03 -0700

    Initial import for cuteco.de

---
+++ cutesite/urls.py
@@ -0,0 +1,63 @@
+from django.conf.urls.defaults import *
+from django.conf import settings
+from django.views.generic.simple import direct_to_template
+from account.openid_consumer import PinaxConsumer
+from django.contrib import admin
+admin.autodiscover()
+
+import os
+
+from microblogging.feeds import TweetFeedAll, TweetFeedUser, TweetFeedUserWithFriends
+tweets_feed_dict = {"feed_dict": {
+    'all': TweetFeedAll,
+    'only': TweetFeedUser,
+    'with_friends': TweetFeedUserWithFriends,
+}}
+
+urlpatterns = patterns('',
+    url(r'^$', direct_to_template, {"template": "homepage.html"}, name="home"),
+
+    (r'^about/', include('about.urls')),
+    (r'^account/', include('account.urls')),
+    (r'^openid/(.*)', PinaxConsumer()),
+    (r'^profiles/', include('profiles.urls')),
+    (r'^notices/', include('notification.urls')),
+    (r'^messages/', include('messages.urls')),
+    (r'^announcements/', include('announcements.urls')),
+    (r'^invitations/', include('friends_app.urls')),
+
+    (r'^avatar/', include('avatar.urls')),
+    (r'^photos/', include('photos.urls')),
+
+    (r'^tweets/', include('microblogging.urls')),
+    (r'^tribes/', include('tribes.urls')),
+
+    (r'^admin/(.*)', admin.site.root),
+
+    (r'^feeds/tweets/(.*)/$', 'django.contrib.syndication.views.feed', tweets_feed_dict),
+)
+
+if settings.SERVE_MEDIA:
+    urlpatterns += patterns('',
+        (r'^site_media/(?P<path>.*)$', 'staticfiles.views.serve')
+    )
+
+
+from microblogging.models import Tweet
+
+friends_tweets_kwargs = {
+    "template_name": "microblogging/friends_tweets.html",
+    "friends_objects_function": lambda users: Tweet.objects.filter(sender_id__in=[user.id for user in users], sender_type__name='user'),
+}
+
+urlpatterns += patterns('',
+#    url('^photos/friends_photos/$', 'friends_app.views.friends_objects', kwargs=friends_photos_kwargs, name="friends_photos"),
+#    url('^blog/friends_blogs/$', 'friends_app.views.friends_objects', kwargs=friends_blogs_kwargs, name="friends_blogs"),
+    url('^tweets/friends_tweets/$', 'friends_app.views.friends_objects', kwargs=friends_tweets_kwargs, name="friends_tweets"),
+#    url('^bookmarks/friends_bookmarks/$', 'friends_app.views.friends_objects', kwargs=friends_bookmarks_kwargs, name="friends_bookmarks"),
+)
+
+if settings.SERVE_MEDIA:
+    urlpatterns += patterns('',
+        (r'^site_media/(?P<path>.*)$', 'staticfiles.views.serve')
+    )
