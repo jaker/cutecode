64da1663dbaa395bbfb1e971c5eb6e957df0e596
Jeff Dlouhy <jeff.dlouhy@gmail.com>
2009-11-27 16:36:11 -0500

    Add initial voting animation for the code detail view.
    Still need to add resets, dialogs for when user is not logged in, and more!

--- cutesite/site_media/static/js/codedetail.js
+++ cutesite/site_media/static/js/codedetail.js
@@ -1,3 +1,8 @@
+var CodeDetail = {};
+var voteUp = $("#doubleplusgood");
+var voteDown = $("#ungood");
+
+
 $(document).ready(function () {
   jQuery.each(jQuery.browser, function(i, val) {
     if(i=="mozilla" && jQuery.browser.version.substr(0,5)=="1.9.0") {
@@ -9,4 +14,48 @@ $(document).ready(function () {
 
   $("input[type='submit']").addClass("small awesome");
 
+  voteUp.click(CodeDetail.chooseVote);
+  voteDown.click(CodeDetail.chooseVote);
 });
+
+
+CodeDetail.chooseVote = function(event) {
+  //most effect types need no options passed by default
+  var options = {};
+  var codeID = $("code.vote-number").attr("id");
+
+  //run the effect
+  if (this.id === "doubleplusgood") {
+    voteDown.fadeOut("fast",CodeDetail.voteCallback($(this)));
+
+
+    var postURL = codeID + "/upvote/";
+    $.post(postURL, null, CodeDetail.updateScore, "json");
+  } else {
+    voteDown.fadeOut("fast",CodeDetail.voteCallback($(this)));
+
+    var postDownURL = codeID + "/downvote/";
+    $.post(postDownURL, null, CodeDetail.updateScore, "json");
+  }
+};
+
+CodeDetail.animateVote = function(element) {
+ element.animate({
+   bottom: "0.5em"}, 500 );
+};
+
+//callback function to bring a hidden box back
+CodeDetail.voteCallback = function(element){
+  CodeDetail.animateVote(element);
+};
+
+CodeDetail.updateScore = function(data){
+  var newScore = data.score;
+
+  if (data.success == true) {
+    var voteNum = $("code.vote-number");
+    voteNum.fadeTo("normal", 0.33);
+    voteNum.html(newScore.score);
+    voteNum.fadeTo("normal", 1.0);
+  }
+};
